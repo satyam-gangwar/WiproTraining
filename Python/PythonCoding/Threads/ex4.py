74eda607d71f3b8e035a5dd3ebbdf7f4f3b9ab74
satyam-gangwar <satyam-gangwar>
2026-05-15 22:46:52 +0530

    threading

---
+++ Python/PythonCoding/Threads/ex4.py
@@ -0,0 +1,14 @@
+from multiprocessing import Process
+def compute(n):
+    result = sum(i*i for i in range(n))
+    print(f"Done computing {n}")
+
+if __name__ == '__main__':
+    numbers = [10**6, 10**7]
+    processes = [Process(target=compute, args=(n,)) for n in numbers]
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
